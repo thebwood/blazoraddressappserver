b8b719af06709849c9215eaaabf2a68398f9e31d
Brian David Wood <brian_d_wood@yahoo.com>
2025-02-16 19:29:35 -0500

    Adding loading spinner on addresses page

--- AddressApp/AddressAppServer/AddressAppServer.Web/Components/Pages/Addresses/AddressesPage.razor.cs
+++ AddressApp/AddressAppServer/AddressAppServer.Web/Components/Pages/Addresses/AddressesPage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using AddressAppServer.Web.BaseClasses;
 using AddressAppServer.Web.ViewModels.Addresses;
+using AddressAppServer.Web.ViewModels.Common;
 using Microsoft.AspNetCore.Components;
 
 namespace AddressAppServer.Web.Components.Pages.Addresses
@@ -8,13 +9,23 @@ namespace AddressAppServer.Web.Components.Pages.Addresses
     {
         [Inject]
         private AddressesViewModel AddressesViewModel { get; set; }
+        [Inject]
+        private UIStateViewModel _viewModel { get; set; } = default!;
 
         [Inject]
         private ILogger<AddressesPage> Logger { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            await AddressesViewModel.GetAddresses();
+            try
+            {
+                _viewModel.IsLoading = true;
+                await AddressesViewModel.GetAddresses();
+            }
+            finally
+            {
+                _viewModel.IsLoading = false;
+            }
         }
     }
 }
